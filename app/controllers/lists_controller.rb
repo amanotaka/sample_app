b6fcaab412665777ddf6085c6b081c3cb688a3bb
amanotaka <amanokuma@gmail.com>
2023-08-27 10:46:51 +0000

    12SYOfinish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,12 +3,17 @@ class ListsController < ApplicationController
     @list = List.new  # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
   end
 
-  def create
+ def create
+    # １. データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
       redirect_to list_path(@list.id)
     else
-      @lists = List.all
+      # 4. フラッシュメッセージを定義し、new.html.bを描画する
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
